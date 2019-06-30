9fc149c28cd667fac5c0ee405440889e53af5aee
jeff-gosselin <jgoz.email@gmail.com>
2019-06-30 15:39:18 -0400

    implementing routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './css/App.css';
 import Navbar from './components/Navbar';
 import Brand from './components/Brand';
 
-function App() {
-  return (
-    <div className="App">
-		<header>
-			<Navbar />
-			<Brand />
-		</header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<header>
+						<Navbar />
+						<Route path="/" Component={Brand} />
+
+					</header>
+				</div>
+			</Router>
+
+	  );
+	}
 }
 
 export default App;
