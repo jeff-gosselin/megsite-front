9cdb1ad55a72ac26b7943a7a16e4adc1fef4c1dd
jeff-gosselin <jgoz.email@gmail.com>
2019-06-30 13:55:54 -0400

    still working on header

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -6,6 +6,7 @@ const Brand = () => {
 		<div className="name">
         <img src="https://weddingprogramsfast.com/wp-content/uploads/2016/03/M-1-PNG.png" className="initials" />
         <h1>Meghan Gosselin</h1>
+				<p>I am an Art Teacher who lorem ipsums dol lor.</p>
     </div>
 	)
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import '../css/Navbar.css';
-import { FaBeer } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 class Navbar extends Component {
 	render() {
 		return (
 			<nav>
         <ul className="nav-links">
-					<li className="nav-links__one"><FaBeer /></li>
+					<li className="nav-links__one"><a href="#"><FaHome className="home"/></a></li>
           <li><a href="#">About Me</a></li>
           <li><a href="#">My Work</a></li>
           <li><a href="#">Student Work</a></li>
