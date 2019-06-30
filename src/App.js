b22923c9e779758de1aef99d4ea214f8ec4987be
jeff-gosselin <jgoz.email@gmail.com>
2019-06-30 19:27:53 -0400

    setup fade in animation when each page is loaded

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 // CSS
-import './css/App.css';
+import './css/App.scss';
 
 // Components
 import Navbar from './components/Navbar';
@@ -21,19 +21,21 @@ class App extends Component {
 		return (
 
 			<div className="App">
-				<header>
+				<div className="main">
 					<Navbar />
 
-					<Switch>
-						<Route path="/about" component={About} />
-						<Route path="/my-work" component={MyWork} />
-						<Route path="/student-work" component={StudentWork} />
-						<Route path="/login" component={Login} />
-						<Route path="/contact" component={Contact} />
-						<Route path="/" component={Brand} />
-					</Switch>
+					<div className="main__content">
+						<Switch>
+							<Route path="/about" component={About} />
+							<Route path="/my-work" component={MyWork} />
+							<Route path="/student-work" component={StudentWork} />
+							<Route path="/login" component={Login} />
+							<Route path="/contact" component={Contact} />
+							<Route path="/" component={Brand} />
+						</Switch>
+					</div>
 
-				</header>
+				</div>
 				<Footer />
 			</div>
 
