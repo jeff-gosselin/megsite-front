75519f48eeb1197b8ac132659e3b9b49ce22f7b4
jeff-gosselin <jgoz.email@gmail.com>
2019-07-07 13:32:08 -0400

    laying out contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,11 +6,16 @@ import '../../css/Contact.scss';
 function Contact() {
 	return (
 		<div className="contact">
-			<div className="contact__img"></div>
-			<h1 clasName="contact__h1">Contact Me</h1>
+
+			<div className="contact__profile-label">
+				<div className="contact__img"></div>
+				<h1 clasName="contact__h1">Contact Meghan</h1>
+			</div>
+
 			<form className="contact__form">
+
 				<input className="contact__form-email" type="email" name="email" placeholder="Email Address" />
-				<textarea className="contact__form-message" rows="10" cols="30" placeholder="Message" />
+				<textarea className="contact__form-message" rows="8" cols="30" placeholder="Message" />
 			</form>
 		</div>
 	)
