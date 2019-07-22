5105c52946e749c560f2c8ec56f71e4064e60232
jeff-gosselin <jgoz.email@gmail.com>
2019-07-22 14:33:55 -0400

    creating Login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 // CSS
-import '../../css/Contact.scss';
+import '../../css/Login.scss';
 
 function Login() {
 	return (
 		<div className="login">
-			<h2>Login</h2>
+			<div className="login__profile-label">
+				<div className="login__img"></div>
+				<h1 clasName="login__h1">Admin Login</h1>
+			</div>
+
+			<form className="login__form">
+				<input className="login__form-username" type="text" name="username" placeholder="Username" />
+				<input className="login__form-password" type="password" placeholder="Password" />
+			</form>
 		</div>
 	)
 }
